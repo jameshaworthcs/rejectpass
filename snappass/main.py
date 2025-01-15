4398ca4b06439b1c5e2b4e8d873e49b7cb01540d
James Haworth <james@jameshaworth.dev>
2025-01-15 20:05:37 +0000

    Improved styling

--- snappass/main.py
+++ snappass/main.py
@@ -48,7 +48,7 @@ else:
     redis_db = os.environ.get('SNAPPASS_REDIS_DB', 0)
     redis_client = redis.StrictRedis(
         host=redis_host, port=redis_port, db=redis_db)
-REDIS_PREFIX = os.environ.get('REDIS_PREFIX', 'snappass')
+REDIS_PREFIX = os.environ.get('REDIS_PREFIX', 'rejectsecret')
 
 TIME_CONVERSION = {'two weeks': 1209600, 'week': 604800, 'day': 86400,
                    'hour': 3600}
